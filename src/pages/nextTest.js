ff4fd1462d45bd4c3d0d1d077da20604788f4011
Тэмүлэн Галсанпунцаг <23LP2607@G401-CC0119.local>
2024-01-15 11:45:46 +0800

    next initialize

---
+++ src/pages/nextTest.js
@@ -0,0 +1,11 @@
+function nextTest() {
+  return (
+    <div>
+      <ul>NEXT NEXT NEXT</ul>
+      <a href="#"> GOOGLE </a>
+      <ul>NEXT NEXT NEXT</ul>
+      <ul>NEXT NEXT NEXT</ul>
+    </div>
+  );
+}
+export default nextTest;
